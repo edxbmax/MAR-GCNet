84e2c25b430260e0ed3f552c50be60537e5b40b7
xleizs <66953248+edxbmax@users.noreply.github.com>
2025-04-24 11:03:54 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -74,19 +74,3 @@ def compute_AUC(y_true, y_pred):
         class_auc.append(roc_auc_score(y_true[:, i], y_pred[:, i]))
     auc = roc_auc_score(y_true, y_pred)
     return auc, class_auc
-
-# KD loss
-class KdLoss(nn.Module):
-    def __init__(self, alpha, temperature):
-        super(KdLoss, self).__init__()
-        self.alpha = alpha
-        self.T = temperature
-
-    def forward(self, outputs, labels, teacher_outputs):
-        kd_loss = nn.KLDivLoss(reduction='batchmean')(F.log_softmax(outputs / self.T, dim=1),
-                                                      F.softmax(teacher_outputs / self.T, dim=1)) * (
-                          self.alpha * self.T * self.T) + F.binary_cross_entropy_with_logits(outputs, labels) * (
-                          1. - self.alpha)
-        return kd_loss
-
-
