f727f1b92da492ecb98fe2a6964cc3844602a610
xleizs <66953248+edxbmax@users.noreply.github.com>
2025-04-24 11:01:59 +0800

    Update grid.py

--- grid.py
+++ grid.py
@@ -3,7 +3,7 @@ import os
 import random
 import torch
 import torch.nn as nn
-from tqdm import tqdm  # 显示进度条的模块
+from tqdm import tqdm
 import numpy as np
 import pandas as pd
 import pickle
@@ -43,9 +43,7 @@ def weights_init(m):
             nn.init.constant_(m.bias, 0)
     
 def parse_args():
-    # 创建解析器
     parser = argparse.ArgumentParser()
-    # 添加参数
     parser.add_argument('--data-dir', type=str, default='data/ptbxl/', help='Directory for data dir')
     parser.add_argument('--leads', type=str, default='all', help='ECG leads to use')
     parser.add_argument('--seed', type=int, default=42, help='Seed to split data')
@@ -54,73 +52,70 @@ def parse_args():
     parser.add_argument('--batch-size', type=int, default=64, help='Batch size')
     parser.add_argument('--num-workers', type=int, default=4, help='Num of workers to load data')
     parser.add_argument('--phase', type=str, default='train', help='Phase: train or test')
-    parser.add_argument('--epochs', type=int, default=100, help='Training epochs')
+    parser.add_argument('--epochs', type=int, default=500, help='Training epochs')
     parser.add_argument('--resume', default=False, action='store_true', help='Resume')
     parser.add_argument('--use-gpu', default=True, action='store_true', help='Use GPU')
     parser.add_argument('--model-path', type=str, default='', help='Path to saved model')
-    return parser.parse_args()  # 解析参数
+    return parser.parse_args()
 
 # 训练
 def train(dataloader, net, args, criterion, epoch, scheduler, optimizer, device):
     print('Training epoch %d:' % epoch)
-    net.train()  # 启用 BatchNormalization 和 Dropout。 在模型测试阶段使用model.train() 让model变成训练模式，此时 dropout和batch normalization的操作在训练q起到防止网络过拟合的问题。
+    net.train() 
     running_loss = 0
     output_list, labels_list = [], []
     for _, (data, labels) in enumerate(tqdm(dataloader)):
         data, labels = data.to(device), labels.to(device)
-        output = net(data)  # 输出 前向传播
-        loss = criterion(output, labels)  # 损失
-        optimizer.zero_grad()  # 梯度清零
-        loss.backward()  # 反向传播
+        output = net(data) 
+        loss = criterion(output, labels) 
+        optimizer.zero_grad() 
+        loss.backward() 
         torch.nn.utils.clip_grad_norm_(parameters=net.parameters(), max_norm=10, norm_type=2)
-        optimizer.step()  # 优化
-        running_loss += loss.item()  # 损失值
+        optimizer.step() 
+        running_loss += loss.item() 
         output_list.append(output.data.cpu().numpy())
         labels_list.append(labels.data.cpu().numpy())
 ##    scheduler.step()
     print('Loss: %.4f' % (running_loss/len(dataloader)))
 
-# 验证
 def evaluate(dataloader, net, args, criterion, device):
     print('Validating...')
-    net.eval()  # 不启用 BatchNormalization 和 Dropout。此时pytorch会自动把BN和DropOut固定住，不会取平均，而是用训练好的值。
+    net.eval() 
     running_loss = 0
     output_list, labels_list = [], []
     for _, (data, labels) in enumerate(tqdm(dataloader)):
         data, labels = data.to(device), labels.to(device)
-        output = net(data)  # 输出
-        loss = criterion(output, labels)  # 损失
-        running_loss += loss.item()  # 损失值
-        output = torch.sigmoid(output)  # sigmoid
+        output = net(data) 
+        loss = criterion(output, labels) 
+        running_loss += loss.item() 
+        output = torch.sigmoid(output) 
         output_list.append(output.data.cpu().numpy())
         labels_list.append(labels.data.cpu().numpy())
     print('Loss: %.4f' % (running_loss/len(dataloader)))
-    y_trues = np.vstack(labels_list)  # 正确标签
-    y_scores = np.vstack(output_list)  # 得分
+    y_trues = np.vstack(labels_list) 
+    y_scores = np.vstack(output_list) 
     AUC, _ = compute_AUC(y_trues, y_scores)
     F1 = compute_F1(y_trues, y_scores)
     print('F1:%.4f, AUC: %.4f' % (F1, AUC))
-    # 判断是否变好
     if args.phase == 'train' and F1 >= args.best_metric:
         args.best_metric = F1
-        torch.save(net.state_dict(), args.model_path)  # 保存当前最佳模型
+        torch.save(net.state_dict(), args.model_path)  
     else:
         pass
 
-# 测试
 def test(dataloader, net, args, device):
     print('Testing...')
-    net.eval()  # 不启用 BatchNormalization 和 Dropout。此时pytorch会自动把BN和DropOut固定住，不会取平均，而是用训练好的值。
+    net.eval() 
     running_loss = 0
     output_list, labels_list = [], []
     for _, (data, labels) in enumerate(tqdm(dataloader)):
         data, labels = data.to(device), labels.to(device)
-        output = net(data)  # 输出
-        output = torch.sigmoid(output)  # sigmoid
+        output = net(data) 
+        output = torch.sigmoid(output)
         output_list.append(output.data.cpu().numpy())
         labels_list.append(labels.data.cpu().numpy())
-    y_trues = np.vstack(labels_list)  # 正确标签
-    y_scores = np.vstack(output_list)  # 得分
+    y_trues = np.vstack(labels_list) 
+    y_scores = np.vstack(output_list) 
     AUC, _ = compute_AUC(y_trues, y_scores)
     TPR = compute_TPR(y_trues, y_scores)
     F1 = compute_F1(y_trues, y_scores)
@@ -133,22 +128,22 @@ def test(dataloader, net, args, device):
 
 def train2(dataloader, net, args, criterion1, criterion2, epoch, scheduler, optimizer, device, b):
     print('Training epoch %d:' % epoch)
-    net.train()  # 启用 BatchNormalization 和 Dropout。 在模型测试阶段使用model.train() 让model变成训练模式，此时 dropout和batch normalization的操作在训练q起到防止网络过拟合的问题。
+    net.train() 
     running_loss1 = 0
     running_loss2 = 0
     output_list, labels_list = [], []
     for _, (data, labels) in enumerate(tqdm(dataloader)):
         data, labels = data.to(device), labels.to(device)
-        output1,output2 = net(data)  # 输出 前向传播
-        loss1 = criterion1(output1, labels)  # 损失
+        output1,output2 = net(data) 
+        loss1 = criterion1(output1, labels) 
         loss2 = criterion2(output2, labels)
-        optimizer.zero_grad()  # 梯度清零
+        optimizer.zero_grad() 
         loss = b*loss1 + (1-b)*loss2
         loss.backward()
         torch.nn.utils.clip_grad_norm_(parameters=net.parameters(), max_norm=10, norm_type=2)
-        optimizer.step()  # 优化
-        running_loss1 += loss1.item()  # 损失值
-        running_loss2 += loss2.item()  # 损失值
+        optimizer.step()  
+        running_loss1 += loss1.item() 
+        running_loss2 += loss2.item()  
         output = b*torch.sigmoid(output1) + (1-b)*torch.sigmoid(output2)
         output_list.append(output.data.cpu().numpy())
         labels_list.append(labels.data.cpu().numpy())
@@ -158,47 +153,46 @@ def train2(dataloader, net, args, criterion1, criterion2, epoch, scheduler, opti
 
 def evaluate2(dataloader, net, args, criterion1, criterion2, device, b):
     print('Validating...')
-    net.eval()  # 不启用 BatchNormalization 和 Dropout。此时pytorch会自动把BN和DropOut固定住，不会取平均，而是用训练好的值。
+    net.eval() 
     running_loss1 = 0
     running_loss2 = 0
     output_list, labels_list = [], []
     for _, (data, labels) in enumerate(tqdm(dataloader)):
         data, labels = data.to(device), labels.to(device)
-        output1,output2 = net(data)  # 输出
-        loss1 = criterion1(output1, labels)  # 损失
+        output1,output2 = net(data)  
+        loss1 = criterion1(output1, labels) 
         loss2 = criterion2(output2, labels)
-        running_loss1 += loss1.item()  # 损失值
-        running_loss2 += loss2.item()  # 损失值
+        running_loss1 += loss1.item() 
+        running_loss2 += loss2.item() 
         output = b*torch.sigmoid(output1) + (1-b)*torch.sigmoid(output2)
         output_list.append(output.data.cpu().numpy())
         labels_list.append(labels.data.cpu().numpy())
     print('Loss1: %.4f' % (running_loss1/len(dataloader)))
     print('Loss2: %.4f' % (running_loss2/len(dataloader)))
-    y_trues = np.vstack(labels_list)  # 正确标签
-    y_scores = np.vstack(output_list)  # 得分
+    y_trues = np.vstack(labels_list) 
+    y_scores = np.vstack(output_list) 
     AUC, _ = compute_AUC(y_trues, y_scores)
     F1 = compute_F1(y_trues, y_scores)
     print('F1:%.4f, AUC: %.4f' % (F1, AUC))
-    # 判断是否变好
     if args.phase == 'train' and F1 >= args.best_metric:
         args.best_metric = F1
-        torch.save(net.state_dict(), args.model_path)  # 保存当前最佳模型
+        torch.save(net.state_dict(), args.model_path) 
     else:
         pass
 
 def test2(dataloader, net, args, device, b):
     print('Testing...')
-    net.eval()  # 不启用 BatchNormalization 和 Dropout。此时pytorch会自动把BN和DropOut固定住，不会取平均，而是用训练好的值。
+    net.eval() 
     running_loss = 0
     output_list, labels_list = [], []
     for _, (data, labels) in enumerate(tqdm(dataloader)):
         data, labels = data.to(device), labels.to(device)
-        output1, output2 = net(data)  # 输出
+        output1, output2 = net(data) 
         output = b*torch.sigmoid(output1) + (1-b)*torch.sigmoid(output2)
         output_list.append(output.data.cpu().numpy())
         labels_list.append(labels.data.cpu().numpy())
-    y_trues = np.vstack(labels_list)  # 正确标签
-    y_scores = np.vstack(output_list)  # 得分
+    y_trues = np.vstack(labels_list) 
+    y_scores = np.vstack(output_list) 
     AUC, _ = compute_AUC(y_trues, y_scores)
     TPR = compute_TPR(y_trues, y_scores)
     F1 = compute_F1(y_trues, y_scores)
@@ -210,7 +204,6 @@ def test2(dataloader, net, args, device, b):
         df.to_csv(args.result_path)
         
         df = pd.DataFrame([[args.hyperparameter, round(AUC,4), round(TPR,4), round(F1,4), round(ACC,4)]], columns=["hyperparameter", "AUC", "TPR", "F1", "ACCs"])
-        # 追加到文件
         df.to_csv(args.results_path, index=False, mode="a", header=False, encoding="utf-8")
 
 
@@ -245,22 +238,18 @@ def choose_model(models, nleads, num_classes, device, adj_file=None, inp=None, t
 if __name__ == "__main__":
     
     args = parse_args()
-    data_dir = args.data_dir  # 规范化指定路径  data/ptbxl/
-    
+    data_dir = args.data_dir 
     args.data_dir='data/ptbxl/'
     
-
-    # 是否使用GPU
     if args.use_gpu and torch.cuda.is_available():
         device = torch.device('cuda:0')
     else:
         device = 'cpu'
     print(device)
 
-    # 选择导联数量
     if args.leads == 'all':
-        leads = 'all'  # 所有导联
-        nleads = 12  # 导联数量
+        leads = 'all'  
+        nleads = 12  
     else:
         leads = args.leads.split(',')
         nleads = len(leads)
@@ -294,17 +283,13 @@ if __name__ == "__main__":
             
     for name, task in experiments:
         task_folder_path = f'results/{task}'
-        # 判断文件夹是否存在
         if not os.path.exists(task_folder_path):
-            # 文件夹不存在，创建文件夹
             os.makedirs(task_folder_path)
 
         for model in models:
             set_seed(args)
             model_folder_path = f'results/{task}/{model}'
-            # 判断文件夹是否存在
             if not os.path.exists(model_folder_path):
-                # 文件夹不存在，创建文件夹
                 os.makedirs(model_folder_path)
                 
             # datasets
@@ -324,14 +309,13 @@ if __name__ == "__main__":
                     print(task, model)
                     args.hyperparameter = f't{t}b{b}'
                     print(args.hyperparameter)
-                    args.best_metric = 0  # 记录最高标准
-                    # 模型
+                    args.best_metric = 0 
+                  
                     net = choose_model(model, nleads, num_classes, device, adj_file, inp, t)
-                    optimizer = torch.optim.Adam(net.parameters(), lr=args.lr)  # adam优化器
-                    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [10,], gamma=0.1)  # 每隔10个epoch，将学习率衰减（lr*gamma）
-                    criterion = nn.BCEWithLogitsLoss()  # 损失函数
+                    optimizer = torch.optim.Adam(net.parameters(), lr=args.lr)  
+                    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [10,], gamma=0.1)  
+                    criterion = nn.BCEWithLogitsLoss()  
                     
-                    # 模型路径
                     if t == None:
                         args.model_path = f'results/{task}/{model}/{model}.pth'
                         args.result_path = f'results/{task}/{model}/{model}.csv'
@@ -347,23 +331,18 @@ if __name__ == "__main__":
                             args.threshold_path = f'results/{task}/{model}/{model}_t{t}b{b}.pkl'
                             args.results_path = f'results/{task}/{model}/{model}.csv'
                             
-                            # 定义表头
                             headers = ["hyperparameter", "AUC", "TPR", "F1", "ACCs"]
-                            # 如果文件不存在，创建文件并写入表头
                             if not os.path.exists(args.results_path):
-                                # 创建一个空的DataFrame并写入表头
                                 pd.DataFrame(columns=headers).to_csv(args.results_path, index=False, mode="w", encoding="utf-8")
                     
-                    # 训练
                     args.phase = 'train'
                     if args.resume:
-                        # load_state_dict 加载模型参数  load加载模型
                         net.load_state_dict(torch.load(args.model_path, map_location=device))
                     else:
-                        badnum = 0  # 记录没变好次数
-                        epoch = 1  # epoch
+                        badnum = 0  
+                        epoch = 1  
                         flag = args.best_metric
-                        while badnum < 10:  # 停止策略
+                        while badnum < 10: 
                             if b == None:
                                 train(train_loader, net, args, criterion, epoch, scheduler, optimizer, device)
                                 evaluate(val_loader, net, args, criterion, device)
